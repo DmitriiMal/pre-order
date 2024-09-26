929539743c5e286472fcf298dd8ed16cb83ba478
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-09-26 07:50:09 +0200

    added aspect-ratio to the feinkost

--- pages/feinkost.vue
+++ pages/feinkost.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <h1 class="text-sm-h4 text-h5 me-7">Was darf’s sein?</h1>
+    <h1 class="text-sm-h4 text-h5 me-7 mb-4">Was darf’s sein?</h1>
     <v-row dense>
       <!-- <v-col :key="cards[0].title" :cols="cards[0].flex">
-        <v-card>
-          <v-img :src="cards[0].src" class="align-start" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px" cover>
-            <v-card-title class="text-white" v-text="cards[0].title"></v-card-title>
-          </v-img>
-        </v-card>
-      </v-col> -->
+          <v-card>
+            <v-img :src="cards[0].src" class="align-start" gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)" height="200px" cover>
+              <v-card-title class="text-white" v-text="cards[0].title"></v-card-title>
+            </v-img>
+          </v-card>b
+        </v-col> -->
       <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
         <v-card :to="card.link">
-          <v-img :src="card.src" class="align-end" height="200px" cover>
+          <v-img :src="card.src" :class="card.isSpecial ? 'align-start' : 'align-end'" cover aspect-ratio="16/9">
             <v-card-title class="" v-text="card.title"></v-card-title>
           </v-img>
         </v-card>
@@ -29,9 +29,9 @@ function getImageUrl(name, ext) {
 export default {
   data: () => ({
     cards: [
-      { title: 'Special of the day', link: '/products/special', src: getImageUrl('specials', 'png'), flex: 12 },
-      { title: 'BILLA Originals', link: '/products/originals', src: getImageUrl('originals', 'png'), flex: 6 },
-      { title: 'Build your own', link: '/products/custom', src: getImageUrl('custom', 'png'), flex: 6 },
+      { title: 'Special of the day', link: '/products/special', src: getImageUrl('specials', 'png'), flex: 12, isSpecial: true },
+      { title: 'BILLA Originals', link: '/products/originals', src: getImageUrl('originals', 'png'), flex: 6, isSpecial: false },
+      { title: 'Build your own', link: '/products/custom', src: getImageUrl('custom', 'png'), flex: 6, isSpecial: false },
     ],
   }),
 };
