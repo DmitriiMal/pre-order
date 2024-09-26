e64c89b9d8466811113de7bb9353cb1c7663c3c3
Dmitrii Malyshkin <d.w.malyschkin@gmail.com>
2024-09-27 00:17:42 +0200

    style pages/feinkost.vue

--- pages/feinkost.vue
+++ pages/feinkost.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-sm-h4 text-h5 me-7 mb-4">Was darf’s sein?</h1>
+    <h1 class="me-7 mb-4">Was darf’s sein?</h1>
 
     <!-- <v-col :key="cards[0].title" :cols="cards[0].flex">
         <v-card>
@@ -9,20 +9,28 @@
           </v-img>
         </v-card>b
       </v-col> -->
-
     <v-row dense>
       <v-col v-for="card in cards" v-show="card.isSpecial" :key="card.title" :cols="card.flex">
-        <v-car :to="card.link">
-          <v-img :src="card.src" class="align-start" cover aspect-ratio="1.25">
-            <v-card-title class="" v-text="card.title"></v-card-title>
+        <v-card :to="card.link" max-height="400px" max-width="500px">
+          <v-img :src="card.src" cover :aspect-ratio="4 / 3">
+            <v-row style="height: 100%">
+              <v-col align-self="start">
+                <p class="ms-4 mt-4 pb-0 font-weight-regular text-uppercase">Special of the day</p>
+                <v-card-title class="py-0" v-text="card.title"></v-card-title>
+                <v-chip color="red" class="ms-4 px-4"><v-icon icon="mdi-alert-circle" start></v-icon>only 3 left</v-chip>
+              </v-col>
+              <v-col class="d-flex align-center" align-self="end">
+                <h2 class="d-inline me-3">€ 2,39</h2>
+                <v-btn elevation="2" color="amber-lighten-1" icon="mdi-cart-plus"></v-btn>
+              </v-col>
+            </v-row>
           </v-img>
-        </v-car>
+        </v-card>
       </v-col>
     </v-row>
-
     <v-row dense>
       <v-col v-for="card in cards" v-show="!card.isSpecial" :key="card.title" :cols="card.flex">
-        <v-card :to="card.link">
+        <v-card :to="card.link" max-height="310px" max-width="250px">
           <v-img :src="card.src" class="align-end" cover aspect-ratio="0.8">
             <v-card-title class="" v-text="card.title"></v-card-title>
           </v-img>
@@ -41,7 +49,7 @@ function getImageUrl(name, ext) {
 export default {
   data: () => ({
     cards: [
-      { title: 'Special of the day', link: '/products/special', src: getImageUrl('specials', 'png'), flex: 12, isSpecial: true },
+      { title: 'Cordon-Bleu Weckerl', link: '/products/special', src: getImageUrl('specials', 'png'), flex: 12, isSpecial: true },
       { title: 'BILLA Originals', link: '/products/originals', src: getImageUrl('originals', 'png'), flex: 6, isSpecial: false },
       { title: 'Build your own', link: '/products/custom', src: getImageUrl('custom', 'png'), flex: 6, isSpecial: false },
     ],
